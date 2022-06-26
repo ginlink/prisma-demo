2c58a8c3a0b43b4ee7287b6e5686e15344d9c9dc
ginlink <ginlink1998@gmail.com>
2022-06-26 09:59:59 +0800

    feat: complete my graph api

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { ApolloServer } from "apollo-server";
-import { DateTimeResolver } from "graphql-scalars";
-import { Context, context } from "./context";
+import { ApolloServer } from 'apollo-server'
+import { DateTimeResolver } from 'graphql-scalars'
+import { Context, context } from './context'
 
 const typeDefs = `
 type Query {
@@ -15,6 +15,7 @@ type Mutation {
   createDraft(title: String!, content: String, authorEmail: String): Post
   incrementPostViewCount(id: Int!): Post
   deletePost(id: Int!): Post
+  publish(id: Int!): Post
 }
 
 type User {
@@ -36,71 +37,111 @@ type Post {
 }
 
 scalar DateTime
-`;
+`
 
 const resolvers = {
   Query: {
     allUsers: (_parent, _args, context: Context) => {
-      // TODO
+      return context.prisma.user.findMany()
     },
     postById: (_parent, args: { id: number }, context: Context) => {
-      // TODO
+      return context.prisma.user.findUnique({ where: { id: args.id } })
     },
     feed: (
       _parent,
       args: {
-        searchString: string | undefined;
-        skip: number | undefined;
-        take: number | undefined;
+        searchString: string | undefined
+        skip: number | undefined
+        take: number | undefined
       },
       context: Context
     ) => {
-      // TODO
+      return context.prisma.post.findMany({
+        where: {
+          published: true,
+        },
+        skip: args.skip,
+        take: args.take,
+      })
     },
     draftsByUser: (_parent, args: { id: number }, context: Context) => {
-      // TODO
+      return context.prisma.user
+        .findUnique({
+          where: { id: args.id },
+        })
+        .posts({
+          where: {
+            published: false,
+          },
+        })
     },
   },
   Mutation: {
-    signupUser: (
-      _parent,
-      args: { name: string | undefined; email: string },
-      context: Context
-    ) => {
-      // TODO
+    signupUser: (_parent, args: { name: string | undefined; email: string }, context: Context) => {
+      return context.prisma.user.create({
+        data: {
+          name: args.name,
+          email: args.email,
+        },
+      })
     },
     createDraft: (
       _parent,
       args: { title: string; content: string | undefined; authorEmail: string },
       context: Context
     ) => {
-      // TODO
+      return context.prisma.post.create({
+        data: {
+          title: args.title,
+          content: args.content,
+          author: {
+            connect: {
+              email: args.authorEmail,
+            },
+          },
+        },
+      })
     },
-    incrementPostViewCount: (
-      _parent,
-      args: { id: number },
-      context: Context
-    ) => {
-      // TODO
+    publish: (_parent, args: { id: number }, context: Context) => {
+      return context.prisma.post.update({
+        where: { id: args.id },
+        data: {
+          published: true,
+        },
+      })
+    },
+    incrementPostViewCount: (_parent, args: { id: number }, context: Context) => {
+      return context.prisma.post.update({
+        where: { id: args.id },
+        data: {
+          viewCount: {
+            increment: 1,
+          },
+        },
+      })
     },
     deletePost: (_parent, args: { id: number }, context: Context) => {
-      // TODO
+      return context.prisma.post.delete({
+        where: { id: args.id },
+      })
     },
   },
   Post: {
     author: (parent, _args, context: Context) => {
-      return null;
+      return context.prisma.post
+        .findUnique({
+          where: { id: parent.id },
+        })
+        .author()
     },
   },
   User: {
     posts: (parent, _args, context: Context) => {
-      return [];
+      return context.prisma.user.findUnique({ where: { id: parent.id } }).posts()
     },
   },
   DateTime: DateTimeResolver,
-};
+}
 
-const server = new ApolloServer({ typeDefs, resolvers, context });
-server.listen({ port: 4000 }, () =>
-  console.log(`🚀 Server ready at: http://localhost:4000`)
-);
+const server = new ApolloServer({ typeDefs, resolvers, context })
+server.listen({ port: 4000 }, () => console.log(`🚀 Server ready at: http://localhost:4000`))
